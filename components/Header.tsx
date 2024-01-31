8706be94763266f5832f6a2d78c7f958065e69f7
Antrikas <andreas.antoniades17@gmail.com>
2024-01-31 12:17:04 +0200

    booking-clone

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,7 +46,7 @@ const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
          p-6 lg:px-8"
         aria-label="Global"
          >
-            <div className='flex lg:flex-1'>
+            <div className="flex lg:flex-1">
          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Booking.com</span>
             <img
@@ -56,64 +56,129 @@ const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
         </Link>
         </div>
 
-          <div className='flex lg:hidden'>
+          <div className="flex lg:hidden">
             <button
-            type='button'
-            className='-m-2.5 inline-flex items-center justify-center
-            rounded-md p-2.5 text-white'
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center
+            rounded-md p-2.5 text-white"
             onClick={() => setMobileMenuOpen(true)}
             >
-                <span className='sr-only'>Open main menu</span>
-                <Bars3Icon className='h-6 w-6' aria-hidden='true' />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6' aria-hidden='true" />
 
             </button>
             </div> 
 
-            <Popover.Group  className='hidden lg:flex lg:gap-x-12'>
-                    <Popover className='relative'>
+            <Popover.Group  className="hidden lg:flex lg:gap-x-12">
+                    <Popover className="relative">
                         <Popover.Button className="flex items-center gap-x-1 text-sm 
                         font-semibold leading-6 text-white">
                             Stays
                             <ChevronDoubleDownIcon
-                            className='h-5 w-5 flex-none text-white'
-                            aria-hidden='true'
+                            className="h-5 w-5 flex-none text-white"
+                            aria-hidden="true"
                             />
                         </Popover.Button>
 
                     <Transition
                     as={Fragment}
-                    enter='transition ease-out duration-200'
-                    enterFrom='opacity-0 translate-y-1'
-                    enterTo='opacity-100 translate-y-0'
-                    leave='transition ease-in duration-150'
-                    leaveFrom='opacity-100 translate-y-0'
-                    leaveTo='opacity-0 translate-y-1'
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
                     >
-                    <Popover.Panel className='absolute bg-white -left-8
+                    <Popover.Panel className="absolute bg-white -left-8
                     top-full z-1 mt-3 w-screen max-w-md overflow-hidden
-                    rounded-3xl shadow-lg ring-1 ring-gray-900/5'>
-                     <div className='p-4'>
+                    rounded-3xl shadow-lg ring-1 ring-gray-900/5">
+                     <div className="p-4">
                         {products.map((item) => (
                         <div
                         key={item.name}
-                        className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6
-                        hover:bg-gray-50'
+                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6
+                        hover:bg-gray-50"
                         >
-                            <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50
-                            ground-hover:bg-gray-200'>
+                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50
+                            ground-hover:bg-gray-200">
                                 <item.icon
-                            className='h-6 w-6 text-[#013B94] group-hover:text-blue-600'
-                            aria-hidden='true'
+                            className="h-6 w-6 text-[#013B94] group-hover:text-blue-600"
+                            aria-hidden="true"
                             />
                             </div>
+
+                            <div className="flex-auto">
+                                <a
+                                href={item.href}
+                                className="block font-semibold text-[#013B94]"
+                                >
+                                    {item.name}
+                                    <span className="absolute inset-0"/>
+                                </a>
+                                <p className="mt-1 text-[#013B94]">
+                                    {item.description}
+                                </p>
+                                </div>
                             </div>
                         ))}
+                     </div>
+
+                     <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                      {callsToAction.map((item)=> (
+                        <a 
+                        key={item.name}
+                        href={item.href}
+                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6
+                         text-[#013B94] hover:bg-gray-100"
+                        >
+                            <item.icon
+                            className="h-5 w-5 flex-none text-[#013B94]"
+                            aria-hidden="true"
+                            />
+                            {item.name}
+                        </a>
+                      ))}
                      </div>
                     </Popover.Panel>
                     </Transition>
                     </Popover>
-            </Popover.Group>
+
+                        <a href="#" className="text-sm font-semibold leading-6 text-white">
+                            Flights
+                        </a>
+                        <a href="#" className="text-sm font-semibold leading-6 text-white">
+                            Car Rentals
+                        </a>
+                        <a href="#" className="text-sm font-semibold leading-6 text-white">
+                            Attraction
+                        </a>
+                        <a href="#" className="text-sm font-semibold leading-6 text-white">
+                             Flight + Hotel
+                        </a>
+
+               </Popover.Group>
+
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <a href="#" className="text-sm font-semibold leading-6 text-white">
+                        Log in <span aria-hidden="true">&rarr;</span>
+                 </a>
+             </div>        
          </nav>
+
+         <Dialog 
+         as="div"
+         className="lg:hidden"
+         open={mobileMenuOpen}
+         onClose={setMobileMenuOpen}
+         >
+            <div className="fixed inset-0 z-10" />
+
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full
+            overflow-y-auto bg-[#013B94] px-6 py-6 sm: max-w-sm sm:ring-1
+            sm:ring-gray-900/10">
+
+            </Dialog.Panel>
+         </Dialog>
     </header>
   )
 }
